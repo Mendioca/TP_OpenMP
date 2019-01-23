184d4ca8cbecd75a7aec2bd9ec7215709276adc7
Laurent Turpin <laurent.turpin@insa-lyon.fr>
2019-01-23 15:08:03 +0100

    python script to create csv files

---
+++ data_collection.py
@@ -0,0 +1,37 @@
+import sys
+import subprocess
+
+program_name = sys.argv[1]
+max_threads = int(sys.argv[2])
+max_size = int(sys.argv[3])
+
+if "char" in program_name:
+    file = open("results_char.csv", 'w')
+    file.write("size,num_threads,seq_time,naive_parallel_time,smart_parallel_time,task_parallel_time\n")
+else:
+    file = open('results_num.csv', 'w')
+    file.write("size,num_threads,vec_autoAdd,addVectors,multVecByNum,scalarProduct,mat_autoAdd,addMatrices,"
+               "multMatByNum,multMatrices,matByVec\n")
+
+current_size = 100
+
+print(max_size)
+
+while current_size <= max_size:
+    print(f"measuring with size {current_size}")
+    current_num_threads = 1
+    while current_num_threads <= max_threads:
+        print(f"\texecuting program with {current_num_threads} threads")
+        out = subprocess.check_output([program_name, str(current_num_threads), str(current_size)], encoding='utf-8')
+        file.write(out)
+        if current_num_threads == 1:
+            current_num_threads += 1
+        elif current_num_threads < 8:
+            current_num_threads += 2
+        elif current_num_threads < 20:
+            current_num_threads += 4
+        else:
+            current_num_threads += 6
+    current_size *= 2
+
+file.close()
